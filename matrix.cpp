131dacd649286546ec5ad0bb332dd6acdcfdc21b
Nimit1705 <70337805+Nimit1705@users.noreply.github.com>
2022-10-08 13:01:13 +0530

    Update matrix.cpp
    enter a 3x3 matrix and identify highest and lowest element from the matrix

--- matrix.cpp
+++ matrix.cpp
@@ -1,10 +1,7 @@
-// enter a 3x3 matrix and identify highest and lowest elemnt from the matrix
+// enter a 3x3 matrix and identify highest and lowest element from the matrix
 
 #include<iostream>
 using namespace std;
-
-
-// enter a 3x3 matrix and identify highest and lowest elemnt from the matrix
 
 int main(){
    int i,j,mat[3][3],low,hi;
@@ -21,5 +18,25 @@ int main(){
         }
         cout<<endl;
     }
+   
+    low=hi=mat[0][0];
+     for(i=0;i<3;i++){
+         for(j=0;j<3;j++){
+             if(mat[i][j]>hi){
+                 hi=mat[i][j];
+             }
+             else{
+                 if(mat[i][j]<low){
+                     low=mat[i][j];
+
+                }
+            }
+         }
+
+        
+     }
+
+
+ cout<<"highest: "<<hi<<" Lowest: "<<low<<endl;
 
 }
