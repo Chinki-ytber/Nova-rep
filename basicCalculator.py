e9115491616f06f949127caa050fe64f47108eff
Chinki-ytber <88037590+Chinki-ytber@users.noreply.github.com>
2022-10-11 19:44:24 +0530

    Create basicCalculator.py

---
+++ basicCalculator.py
@@ -0,0 +1,25 @@
+#Question of the DAY!
+##complete the code challenge
+
+#place the if/elif statements properly so that when the user give anynumber between 1 to 4 as opt the proper result may come.
+
+
+ch1 = "Y"
+while(ch1 == "Y" or ch1 == "y"):
+
+    print("1. Addition")
+    print("2. subtraction")
+    print("3. multiplication")
+    print("4. division")
+
+    opt = int(input("Enter your selection: "))
+
+    #===========================
+    #your code here
+    #===========================
+
+    else:
+        print("Enter a valid option")
+
+
+    ch1 = input("Do yo want to continue?(Y/N): ")
